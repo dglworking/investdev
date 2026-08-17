a85fa4b04cd846e920f44925f0bd60a5a6166c62
dglworking <dglworking@gmail.com>
2026-08-18 04:22:55 +0700

    update supaase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://mfgywkdbollxzbbtpkig.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_TZic-mhvAyh35i6PMWj1Ng_lbp8M2ma";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
